419ac8dda44023b802f2eb40c20b5bdc5071fd0e
jose luis <2064537+sosan@users.noreply.github.com>
2023-01-24 19:48:21 +0100

    add generic getrequest with httpclient

--- httpclient/responses.go
+++ httpclient/responses.go
@@ -38,7 +38,7 @@ func GeneratePostJsonRequest(uri string, postData []byte) ([]byte, int) {
 }
 
 
-func GenerateGetRequest(uri string,token string) ([]byte, int) {
+func GenerateGetRequest(uri string, token string) ([]byte, int) {
 
 	clientHttp := http.Client{Timeout: time.Duration(60) * time.Second}
 
@@ -86,3 +86,33 @@ func ConfirmAccount(uri string, email string) bool {
 
 }
 
+
+func GetRequest(uri string, token string) ([]byte, int) {
+
+	clientHttp := http.Client{Timeout: time.Duration(60) * time.Second}
+
+	req, _ := http.NewRequest("GET", uri, nil)
+	req.Header.Add("Accept", `application/json`)
+	if token != "" {
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token) )
+	}
+	req.Header.Add("Content-type", "application/json")
+	
+	resp, err := clientHttp.Do(req)
+	
+	if err != nil {
+		log.Fatal(err)
+		return nil, 404
+	}
+	
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		return nil, 404
+	}
+
+	return body, resp.StatusCode
+
+}

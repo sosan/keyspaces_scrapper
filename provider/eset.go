0021f56d24b0852c0ff4cd94ff7dd7c9cc8c7d94
jose luis <2064537+sosan@users.noreply.github.com>
2023-01-24 20:10:28 +0100

    mejor seleccion del modal

--- provider/eset.go
+++ provider/eset.go
@@ -102,7 +102,7 @@ func sendChromeConfirm(email string) (bool, string) {
 
 	ctx, cancel = chromedp.NewContext(ctx, chromedp.WithDebugf(log.Printf))
 	defer cancel()
-
+	// use copyselector in devtools chromium
 	postData := models.PostData{
 		UriValue:               "https://home.eset.com",
 		WaitVisibleValue:       "#fullpage",
@@ -112,15 +112,16 @@ func sendChromeConfirm(email string) (bool, string) {
 		PasswordElement:        "Password",
 		SubmitElement:          "input.account__entry.btn.btn-normal",
 		ButtonHome:             `#main-content > div > ion-tabs > div > ion-router-outlet > div > ion-content > div.home-overview-section > div.home-overview-section__cards-wrap > ion-card > ion-button`,
-		ButtonFreeLicense:      `#license-add-new-slide > form > div.ion-cui-btn-above-link-wrapper > ion-button:nth-child(3)`,
+		ButtonFreeLicense:      `#license-add-new-slides > div > ion-slide.LicenseForkSlide.md.swiper-slide.swiper-zoom-container.hydrated.swiper-slide-active > div > div > ion-card:nth-child(3)`,
 		ButtonWindows:          `#protect-choose-os-9`,
-		ButtonContinue:         `#main-content > div > ion-tabs > div > ion-router-outlet > div > ion-content > div > ion-button`,
+		ButtonContinue:         `#license-add-new-slides > div > ion-slide.LicenseForkSlide.md.swiper-slide.swiper-zoom-container.hydrated.swiper-slide-active > div > ion-button`,
 		EmailToShare:           `#main-content > div > ion-tabs > div > ion-router-outlet > div > ion-content > div > ion-row > form > div > div.ion-cui-form-field > ion-item > div > ion-input > input`,
 		ButtonToSendEmailShare: `#main-content > div > ion-tabs > div > ion-router-outlet > div > ion-content > div > ion-row > form > ion-button`,
 		ButtonShowLicense:      `#license-list-large-previews > ion-row > ion-col > ion-card > ion-button`,
 		TextLicense:            `#main-content > div > ion-tabs > div > ion-router-outlet > div > ion-content > div.license-detail-content > ion-grid.license-detail-portal-content.license-detail-portal-content__bottom-grid.md.hydrated > ion-row > ion-col:nth-child(1) > div > ion-card:nth-child(1) > ion-grid > ion-row:nth-child(2) > ion-col:nth-child(6) > div > div > p.DetailInfoSectionItem__value > ion-text`,
 	}
-
+	// ButtonContinue:         `#main-content > div > ion-tabs > div > ion-router-outlet > div > ion-content > div > ion-button`,
+	// `#license-add-new-slide > form > div.ion-cui-btn-above-link-wrapper > ion-button:nth-child(3)`,
 	var buf []byte
 	var licencia string
 	err := chromedp.Run(ctx, submitConfirmAccount(postData, &buf, &licencia))
@@ -150,8 +151,10 @@ func submitConfirmAccount(postData models.PostData, buf *[]byte, licencia *strin
 		chromedp.Click(postData.ButtonHome, chromedp.ByQuery),
 		chromedp.Sleep(10 * time.Second),
 
-		// click en modal
+		// seleccionar boton prueba gratuita y continuar
 		chromedp.Click(postData.ButtonFreeLicense, chromedp.ByQuery),
+		chromedp.Sleep(2 * time.Second),
+		chromedp.Click(postData.ButtonContinue, chromedp.ByQuery),
 
 		// seleccion windows y continue
 		chromedp.Sleep(5 * time.Second),

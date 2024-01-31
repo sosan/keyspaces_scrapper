254e0b14059c12506ba403a104be7cd0e96c3b17
jose luis <2064537+sosan@users.noreply.github.com>
2024-02-01 00:52:02 +0100

    auto update

--- update/auto.go
+++ update/auto.go
@@ -1,25 +1,50 @@
 package update
 
 import (
+	"fmt"
 	"log"
 	"main/httpclient"
+	"runtime"
 
 	"github.com/minio/selfupdate"
+)
+
+const (
+	URI_DOWNLOAD = `https://github.com/sosan/keyspaces_scrapper/releases/download/latest`
+	OSTIPO = "linux"
 )
 
 func AutoUpdate() (bool, bool) {
 	needUpdate := false
 	updateOk := false
-
-	err := doUpdate("https://github.com/sosan/keyspaces_scrapper/releases/download/main/licencias")
+	
+	uriDownload := getDownloadURI()
+	err := doUpdate(uriDownload)
 	if err != nil {
 		log.Fatalf("ERROR | No es posible conectarse")
 	}
+	log.Printf("%s", OSTIPO)
 
 	return needUpdate, updateOk
 }
 
+func getDownloadURI() string {
+	fileName := getFileName()
+	return fmt.Sprintf("%s/%s", URI_DOWNLOAD, fileName)
+}
 
+func getFileName() string {
+	ostype := getOsType()
+	fileName := "licencias.exe"
+	if ostype == "linux" {
+		fileName = "licencias"
+	}
+	return fileName
+}
+
+func getOsType() string {
+	return runtime.GOOS
+}
 
 func doUpdate(url string) error {
 	statusCode := 404

6462cf5b95d7c1bf6c977979a7c9115353a996bf
jose luis <2064537+sosan@users.noreply.github.com>
2024-02-02 00:09:47 +0100

    minio autoupdate
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -2,22 +2,23 @@ module main
 
 go 1.21
 
-require github.com/chromedp/chromedp v0.9.3
-
-require github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
-
-// require github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+require github.com/chromedp/chromedp v0.9.4
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20231011050154-1d073bb38998 // indirect
+	aead.dev/minisign v0.2.1 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+)
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20240127002248-bd7a66284627 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.3.0 // indirect
+	github.com/gobwas/ws v1.3.2 // indirect
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mouuff/go-rocket-update v1.5.4
-	golang.org/x/sys v0.6.0 // indirect
+	github.com/minio/selfupdate v0.6.0
+	golang.org/x/sys v0.16.0 // indirect
 )
--- update/auto.go
+++ update/auto.go
@@ -1,37 +1,43 @@
 package update
 
 import (
+	"fmt"
 	"log"
-
+	"main/httpclient"
 	"runtime"
-	"github.com/mouuff/go-rocket-update/pkg/provider"
-	"github.com/mouuff/go-rocket-update/pkg/updater"
+
+	"github.com/minio/selfupdate"
 )
 
 const (
-	URI_DOWNLOAD = `github.com/sosan/keyspaces_scrapper`//`https://github.com/sosan/keyspaces_scrapper/releases/download/latest`
-	OSTIPO = "windows"
+	URI_DOWNLOAD = `https://github.com/sosan/keyspaces_scrapper/releases/download/latest`
+	OSTIPO = "linux"
 )
 
 func AutoUpdate() (bool, bool) {
 	needUpdate := false
 	updateOk := false
 	
-	// uriDownload := getDownloadURI()
-	err := doUpdate()
+	uriDownload := getDownloadURI()
+	err := doUpdate(uriDownload)
 	if err != nil {
 		log.Fatalf("ERROR | No es posible conectarse")
 	}
 	log.Printf("%s", OSTIPO)
-	log.Fatalf("TERMINADO")
+
 	return needUpdate, updateOk
+}
+
+func getDownloadURI() string {
+	fileName := getFileName()
+	return fmt.Sprintf("%s/%s", URI_DOWNLOAD, fileName)
 }
 
 func getFileName() string {
 	ostype := getOsType()
-	fileName := "licencias.exe.zip"
+	fileName := "licencias.exe"
 	if ostype == "linux" {
-		fileName = "licencias.zip"
+		fileName = "licencias"
 	}
 	return fileName
 }
@@ -40,37 +46,113 @@ func getOsType() string {
 	return runtime.GOOS
 }
 
-func doUpdate() error {
-	fileName := getFileName()
-	upD := &updater.Updater{
-		Provider: &provider.Github{
-			RepositoryURL: URI_DOWNLOAD,
-			ArchiveName:   fileName,
-		},
-		ExecutableName: fileName,
-		Version:        "v0.0.0",
-	}
+func doUpdate(url string) error {
+	statusCode := 404
+	body, statusCode := httpclient.GetRequestRaw(url, "")
 
-	log.Println(upD.Version)
-	statusCode, err := upD.Update(); 
-	if err != nil {
-		log.Println(err)
-		log.Println(statusCode)
-	}
-
-	// body, statusCode := httpclient.GetRequestRaw(url, "")
-
-    // if statusCode != 200 {
-	// 	log.Fatalf("ERROR | No es posible conectarse")
-	// 	return nil
-    // }
-
-
-    // err := selfupdate.Apply(*body, selfupdate.Options{})
-    // if err != nil {
-    //     // error handling
-	// 	log.Fatalf("ERROR | No es posible conectarse")
-    // }
-	// defer (*body).Close()
+    if statusCode != 200 {
+		log.Fatalf("ERROR | No es posible conectarse")
+		return nil
+    }
+    err := selfupdate.Apply(*body, selfupdate.Options{})
+    if err != nil {
+        // error handling
+		log.Fatalf("ERROR | No es posible conectarse")
+    }
+	defer (*body).Close()
     return err
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package update
+
+// import (
+// 	"log"
+
+// 	"runtime"
+// 	"github.com/mouuff/go-rocket-update/pkg/provider"
+// 	"github.com/mouuff/go-rocket-update/pkg/updater"
+// )
+
+// const (
+// 	URI_DOWNLOAD = `github.com/sosan/keyspaces_scrapper`//`https://github.com/sosan/keyspaces_scrapper/releases/download/latest`
+// 	OSTIPO = "windows"
+// )
+
+// func AutoUpdate() (bool, bool) {
+// 	needUpdate := false
+// 	updateOk := false
+	
+// 	// uriDownload := getDownloadURI()
+// 	err := doUpdate()
+// 	if err != nil {
+// 		log.Fatalf("ERROR | No es posible conectarse")
+// 	}
+// 	log.Printf("%s", OSTIPO)
+// 	log.Fatalf("TERMINADO")
+// 	return needUpdate, updateOk
+// }
+
+// func getFileName() string {
+// 	ostype := getOsType()
+// 	fileName := "licencias.exe.zip"
+// 	if ostype == "linux" {
+// 		fileName = "licencias.zip"
+// 	}
+// 	return fileName
+// }
+
+// func getOsType() string {
+// 	return runtime.GOOS
+// }
+
+// func doUpdate() error {
+// 	fileName := getFileName()
+// 	upD := &updater.Updater{
+// 		Provider: &provider.Github{
+// 			RepositoryURL: URI_DOWNLOAD,
+// 			ArchiveName:   fileName,
+// 		},
+// 		ExecutableName: fileName,
+// 		Version:        "v0.0.0",
+// 	}
+
+// 	log.Println(upD.Version)
+// 	statusCode, err := upD.Update(); 
+// 	if err != nil {
+// 		log.Println(err)
+// 		log.Println(statusCode)
+// 	}
+
+// 	// body, statusCode := httpclient.GetRequestRaw(url, "")
+
+//     // if statusCode != 200 {
+// 	// 	log.Fatalf("ERROR | No es posible conectarse")
+// 	// 	return nil
+//     // }
+
+
+//     // err := selfupdate.Apply(*body, selfupdate.Options{})
+//     // if err != nil {
+//     //     // error handling
+// 	// 	log.Fatalf("ERROR | No es posible conectarse")
+//     // }
+// 	// defer (*body).Close()
+//     return err
+// }
